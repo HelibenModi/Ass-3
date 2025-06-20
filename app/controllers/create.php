e8f1ea2720a0f4e36c799d2bc22afbe7c206bc08
HelibenModi <helimodi710@gmail.com>
2025-06-20 04:56:08 +0000

    Verifying password and confirm password

--- app/controllers/create.php
+++ app/controllers/create.php
@@ -5,4 +5,17 @@ class Create extends Controller {
     public function index() {		
 	    $this->view('create/index');
     }
+  public function register() {
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $username = trim($_POST['username']);
+      $password = $_POST['password'];
+      $confirm_password = $_POST['confirm_password'];
+    
+    if ($password !== $confirm_password) {
+        $_SESSION['error'] = "Passwords do not match.";
+        header('Location: /create');
+        exit;
+    }
+}
+  }
 }
